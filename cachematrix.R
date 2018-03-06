a12c57a28366024729334332ec3a4b03b8412e45
Don Seifried <dons@a-t-g.com>
2018-03-06 15:32:19 -0600

    pa2example.r

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,72 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions show how to take a matrix and cache the inverse of a 
+## matrix so the same calculations don't have to take place multiple times if
+## theinputs are the same
 
-## Write a short comment describing this function
+## Shell Function to build other functions to store a matrix and it's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
+# Initialize matrix(cache) to Null
+      i <- NULL
+      
+# Create the matrix and set cache to Null
+      set <- function(y) {
+            x <<- y
+            i <<- NULL
+      }
+      
+## Get Matrix
+      get <- function() x
+## Store Inverse of Matrix in Cache
+      setinverse <- function(inverse) i <<- inverse
+## Get Cache Value
+      getinverse <- function() i
+## Name function to be able to call from environment
+      list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the cache matrix if it is not 
+## already in cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+      i <- x$getinverse()
+## If cache it NOT empty, return cache value and exit function      
+      if(!is.null(i)) {
+            message("getting cached data")
+            return(i)
+      }
+## If cache is empty, get new matrix value, set Inverse, and return inverse
+## matrix value
+
+      data <- x$get()
+      i <- solve(data)
+      x$setinverse(i)
+      return(i)
 }
+
+# Testing Results
+
+# > source('P:/RScripts/ProgrammingAssignment2/cachematrix.R')
+# > a<-matrix(1:4,2,2)
+# > b<-makeCacheMatrix(a)
+# > a
+# [,1] [,2]
+# [1,]    1    3
+# [2,]    2    4
+# > b$get()
+# [,1] [,2]
+# [1,]    1    3
+# [2,]    2    4
+# Data is not cached
+# > cacheSolve(b)
+# [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
+# > cacheSolve(b)
+# Data is cached
+# getting cached data
+# [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
--- pa2example.r
+++ pa2example.r
@@ -14,7 +14,7 @@ makeVector <- function(x = numeric()) {
 
 cachemean <- function(x, ...) {
       m <- x$getmean()
-      if(!is.null()) {
+      if(!is.null(m)) {
             message("getting cached data")
             return(m)
       }
@@ -37,3 +37,44 @@ cachemean <- function(x, ...) {
 #       ## Return a matrix that is the inverse of 'x'
 # }
 
+# > rm(list=ls())
+# > source('P:/RScripts/ProgrammingAssignment2/pa2example.r')
+
+# > a<-c(5,6,7,8,9)
+# > a
+# [1] 5 6 7 8 9
+
+# > b<-makeVector(a)
+
+# > b
+# $set
+# function (y) 
+# {
+#       x <<- y
+#       m <<- NULL
+# }
+# <environment: 0x00000000068dd948>
+#       
+#       $get
+# function () 
+#       x
+# <environment: 0x00000000068dd948>
+#       
+#       $setmean
+# function (mean) 
+#       m <<- mean
+# <environment: 0x00000000068dd948>
+#       
+#       $getmean
+# function () 
+#       m
+# <environment: 0x00000000068dd948>
+#       
+# > b$get()
+# [1] 5 6 7 8 9
+# Data Not Cached
+#> cachemean(b)
+# Data Cached
+# > cachemean(b)
+# getting cached data
+# [1] 7
